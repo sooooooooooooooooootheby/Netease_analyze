247400934bfbc01cac3d05839d6d88df7d967de1
Sooooooooooooooooootheby <120046369+sooooooooooooooooootheby@users.noreply.github.com>
2025-03-26 23:35:40 +0800

    feat(./ecosystem.config.cjs): 添加了PM2支持

---
+++ ecosystem.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Netease_analyze',
+      port: '3002',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs'
+    }
+  ]
+}
