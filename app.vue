013cab2a96b9504f4d0ec8d27384486f933d7912
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-05-26 20:22:13 +0800

    feat: 恢复了解析计数器

--- app.vue
+++ app.vue
@@ -5,16 +5,16 @@
         <notification />
         <div class="btn fixed right-5 bottom-4" @click="navigateTo('https://github.com/sooooooooooooooooootheby/Netease_analyze', { open: { target: '_blank' } })">
             <Icon name="akar-icons:github-fill" />
-            <!-- {{ count }} -->
+            {{ count }}
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-// const count = ref<number>(0);
+const count = ref<number>(0);
 
-// onMounted(async () => {
-//     count.value = await $fetch("https://api.s22y.moe/count/get?name=neteasy&type=number");
-// });
+onMounted(async () => {
+    count.value = await $fetch("https://api.s22y.moe/count/get?name=neteasy&type=number");
+});
 </script>
 
